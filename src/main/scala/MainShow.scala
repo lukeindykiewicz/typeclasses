aaa09b1f73ddda922dd0f5f15322c9184811d611
Łukasz Indykiewicz <lukasz.indykiewicz@scalac.io>
2017-03-13 13:37:09 +0100

    Add some own type

--- src/main/scala/MainShow.scala
+++ src/main/scala/MainShow.scala
@@ -5,8 +5,14 @@ import show.Show._
 
 object MainShow extends App {
 
+  case class Foo(foo: Int)
+
+  implicit val fooShow: Show[Foo] =
+    foo => s"case class Foo(foo: ${foo.foo})"
+
   println(show(20))
   println(30.show)
   println("foofoo".show)
+  println(Foo(42).show)
 
 }
