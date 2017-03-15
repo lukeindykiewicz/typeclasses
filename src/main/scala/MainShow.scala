4e26bff3f231091db5a01910d3e647bad40e270a
Łukasz Indykiewicz <lukasz.indykiewicz@scalac.io>
2017-03-15 15:43:42 +0100

    Add ops object

--- src/main/scala/MainShow.scala
+++ src/main/scala/MainShow.scala
@@ -1,7 +1,7 @@
 package mainshow
 
 import show.Show
-import show.Show._
+import show.Show.ops._
 
 //for simulacrum based implementation
 import showsimulacrum.ShowSim
@@ -14,9 +14,6 @@ object MainShow extends App {
   implicit val fooShow: Show[Foo] =
     foo => s"case class Foo(foo: ${foo.foo})"
 
-  val hipsterString: Show[String] =
-    str => s"""hipster string "$str"."""
-
   println(show(20))
   println(30.show)
   println("foofoo".show)
@@ -24,7 +21,13 @@ object MainShow extends App {
 
   println("bar".showSim)
 
-  println("baz".showExp)
-  println("baz".showExp(hipsterString))
+  println("baz".show)
+
+  {
+    implicit val hipsterString: Show[String] =
+      str => s"""hipster string "$str"."""
+
+    println("bazbaz".show)
+  }
 
 }
--- src/main/scala/Show.scala
+++ src/main/scala/Show.scala
@@ -8,11 +8,12 @@ object Show {
 
   def apply[A](implicit sh: Show[A]): Show[A] = sh
 
-  def show[A: Show](a: A) = Show[A].show(a)
+  object ops {
+    def show[A: Show](a: A) = Show[A].show(a)
 
-  implicit class ShowOps[A: Show](a: A) {
-    def show = Show[A].show(a)
-    def showExp(implicit sh: Show[A]) = sh.show(a)
+    implicit class ShowOps[A: Show](a: A) {
+      def show = Show[A].show(a)
+    }
   }
 
   implicit val intCanShow: Show[Int] =
